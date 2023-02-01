8ddf1fbffd1fce46b01a1f2ab80f24dc3f8fa1a9
Sami Kukkonen <samikukkonen00@hotmail.com>
2023-02-01 20:16:49 +0200

    Register working

--- routes/transaction.js
+++ routes/transaction.js
@@ -2,12 +2,10 @@ const express = require('express')
 const router = express.Router();
 const pool = require('../helpers/database');
 
-router.get('/:id', async (req,res) =>{
+router.get('/', async (req,res) =>{
     try{
-        const sqlQuery = `SELECT TransactionName, TransactionRepeat, Recipient
-FROM transaction 
-WHERE transaction.SubCategoryID = (SELECT subcategory.SubCategoryID FROM subcategory WHERE subcategory.SubCategoryName =?)`;
-        const rows = await pool.query(sqlQuery, req.params.id);
+        const sqlQuery = `SELECT TransactionName, TransactionRepeat, Recipient, transaction.Outflow, transaction.Memo FROM transaction`;
+        const rows = await pool.query(sqlQuery);
         res.status(200).json(rows);
     }catch (error){
         res.status(400).send(error.message)
