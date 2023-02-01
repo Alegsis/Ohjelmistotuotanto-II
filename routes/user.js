79d3fa44823115c9d9284b073c67933b3bcc3708
Sami Kukkonen <samikukkonen00@hotmail.com>
2023-02-01 22:24:46 +0200

    Login working and password encrypting enabled

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router();
 const pool = require('../helpers/database');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
-router.get('/:id', async (req,res) =>{
+router.get('/:id', async (req,res) => {
     try{
         const sqlQuery = `SELECT UserID, Username, Email FROM user WHERE userID=?`;
         const rows = await pool.query(sqlQuery, req.params.id);
@@ -15,12 +17,33 @@ router.get('/:id', async (req,res) =>{
 router.post('/register', async (req, res) => {
     try{
         const {username, email, password} = req.body;
+        const encryptedPassword = await bcrypt.hash(password, saltRounds)
+
 
         const sqlQuery = 'INSERT INTO user (Username, Email, UserPassword) VALUES (?, ?, ?)';
-        const result = await pool.query(sqlQuery, [username, email, password]);
+        const result = await pool.query(sqlQuery, [username, email, encryptedPassword]);
 
-        res.status(200).json(result)
+        res.status(200).json({userID: result.insertId})
     } catch (error){
+        res.status(400).send(error.message)
+    }
+})
+router.post('/login', async (req,res) => {
+    try {
+        const {username, password} = req.body;
+
+        const sqlGetUser = 'SELECT UserPassword FROM user WHERE Username=?';
+        const rows = await pool.query(sqlGetUser, username);
+
+        const isValid = await bcrypt.compare(password, rows[0].UserPassword)
+
+        if(isValid){
+            res.status(200).json({valid_password: isValid});
+        } else if (!isValid){
+            res.status(200).send('Wrong password');
+        }
+
+    } catch (error) {
         res.status(400).send(error.message)
     }
 })
