6cee513553f4d5926af4101df3c3bafa38cb6433
Pionyx <110421861+Pionyx@users.noreply.github.com>
2023-02-01 20:00:24 +0200

    Initial front

---
+++ budgeting_app/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Sidebar from './components/Sidebar';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Category from './pages/Categories';
+import Transaction from "./pages/Transaction";
+import Accounts from "./pages/Account";
+
+
+function App() {
+  return (
+      <Router>
+          <Sidebar />
+          <Routes>
+              <Route path="/dashboard" element={<Dashboard/>} />
+              <Route path="/dashboard/categories" element={<Category/>} />
+              <Route path="/dashboard/transaction" element={<Transaction/>} />
+              <Route path="/accounts" element={<Accounts/>} />
+          </Routes>
+      </Router>
+  );
+};
+
+export default App;
---
+++ budgeting_app/src/components/SubMenu.js
@@ -0,0 +1,72 @@
+import React, {useState} from 'react'
+import {Link} from 'react-router-dom'
+import styled from "@emotion/styled"
+
+const SidebarLink = styled(Link)`
+  display: flex;
+  color: #e1e9fc;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  height: 60px;
+  text-decoration: none;
+  font-size: 18px;
+  border-left: 4px solid #15171c;
+
+  &:hover {
+    background: #252831;
+    border-left: 4px solid #d380ff;
+    cursor: pointer;
+  }
+`;
+
+const SidebarLabel = styled.span`
+  margin-left: 16px;
+`;
+
+const DropdownLink = styled(Link)`
+  height: 60px;
+  padding-left: 3rem;
+  background: #15171c;
+  display: flex;
+  text-decoration: none;
+  align-items: center;
+  color: #ffffff;
+  font-size: 18px;
+  border-left: 4px solid #15171c;
+
+  &:hover{
+    background: #252831;
+    border-left: 4px solid #d380ff;
+    cursor: pointer;
+  }
+  
+`;
+
+const Submenu = ({ item }) => {
+    const [subnav, setSubnav] = useState(false);
+    const showSubnav = () => setSubnav(!subnav);
+    return (
+        <>
+        <SidebarLink to={item.path} onClick={item.subNavi && showSubnav}>
+            <div>
+                {item.icon}
+                <SidebarLabel>{item.title}</SidebarLabel>
+            </div>
+            <div>
+                {item.subNavi && subnav ? item.iconOpen : item.subNavi ? item.iconClosed : null}
+            </div>
+        </SidebarLink>
+            {subnav && item.subNavi.map((item, index) => {
+            return (
+                <DropdownLink to={item.path} key={index}>
+                    {item.icon}
+                    <SidebarLabel>{item.title}</SidebarLabel>
+                </DropdownLink>
+            )
+            })}
+        </>
+    );
+};
+
+export default  Submenu
