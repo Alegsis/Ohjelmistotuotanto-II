02c3135a66ff4f18545cb6771a3e3b23ebe14557
Antti H <akthurme@gmail.com>
2023-02-15 20:00:25 +0200

    new component mount

--- budgeting_app/src/components/Header.js
+++ budgeting_app/src/components/Header.js
@@ -1,16 +1,15 @@
 import * as React from 'react';
-import styled from "@emotion/styled";
-import MenuIcon from "@mui/icons-material/Menu";
-import { Link } from "react-router-dom";
-import Login from "./Login";
-
-
-
+import styled from '@emotion/styled';
+import MenuIcon from '@mui/icons-material/Menu';
+import {Link} from 'react-router-dom';
+import Login from './Login';
+import Total from './Total';
 
 const HeaderNavi = styled.div`
   display: flex;
   justify-content: flex-start;
   align-items: center;
+  color: white;
 `;
 
 const NaviIcon = styled(Link)`
@@ -30,13 +29,14 @@ const showSidebar = () =>  {
 */
 
 const Header = () => {
-    return (
-    <HeaderNavi>
+  return (
+      <HeaderNavi>
         <NaviIcon to="#">
-            <MenuIcon/>
+          <MenuIcon/>
         </NaviIcon>
+        <Total></Total>
         <Login></Login>
-    </HeaderNavi>
-    )
-}
+      </HeaderNavi>
+  );
+};
 export default Header;
