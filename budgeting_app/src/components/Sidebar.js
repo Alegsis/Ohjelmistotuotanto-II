427ea0eb59427e27e28ff3b2d8aa3eb8f17df80e
Antti H <akthurme@gmail.com>
2023-02-16 10:32:16 +0200

    remove more unnecessary imports

--- budgeting_app/src/components/Sidebar.js
+++ budgeting_app/src/components/Sidebar.js
@@ -1,11 +1,9 @@
-import React, {useState} from 'react';
+import React from 'react';
 import styled from '@emotion/styled';
-import MenuIcon from '@mui/icons-material/Menu';
-import CloseIcon from '@mui/icons-material/Close';
-import {Link} from 'react-router-dom';
 import {SidebarData} from './SidebarData';
-// import * as IconsMaterial from '@mui/icons-material/'; // malli importata kaikki ikonit
 import Submenu from './SubMenu';
+
+// import * as IconsMaterial from '@mui/icons-material/'; // malli importata kaikki ikonit
 
 const SidebarWrap = styled.nav`
   width: 100%;
