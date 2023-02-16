7fd218203fe6d0b8d5556deb2068a3a1096e4bd2
Antti H <akthurme@gmail.com>
2023-02-16 10:30:24 +0200

    remove unnecessary imports

--- budgeting_app/src/components/Sidebar.js
+++ budgeting_app/src/components/Sidebar.js
@@ -1,18 +1,11 @@
-import React, { useState } from "react";
-import styled from "@emotion/styled";
-import MenuIcon from "@mui/icons-material/Menu";
-import CloseIcon from "@mui/icons-material/Close";
-import { Link } from "react-router-dom";
-import { SidebarData } from "./SidebarData";
+import React, {useState} from 'react';
+import styled from '@emotion/styled';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+import {Link} from 'react-router-dom';
+import {SidebarData} from './SidebarData';
 // import * as IconsMaterial from '@mui/icons-material/'; // malli importata kaikki ikonit
-import Submenu from "./SubMenu";
-import Login from "./Login";
-import CreateBankAcc from "./CreateBankAcc";
-import AddCircleOutline from "@mui/icons-material/AddCircleOutline";
-
-
-
-
+import Submenu from './SubMenu';
 
 const SidebarWrap = styled.nav`
   width: 100%;
@@ -21,11 +14,11 @@ const SidebarWrap = styled.nav`
 const Sidebar = () => {
   return (
 
-        <SidebarWrap>
-          {SidebarData.map((item, index) => {
-            return <Submenu item={item} key={index} />;
-          })}
-        </SidebarWrap>
+      <SidebarWrap>
+        {SidebarData.map((item, index) => {
+          return <Submenu item={item} key={index}/>;
+        })}
+      </SidebarWrap>
 
   );
 };
