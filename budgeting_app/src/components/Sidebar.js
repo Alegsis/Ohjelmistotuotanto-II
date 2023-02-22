9b32883a42c02f6d466df5229a7a62a1ea8441aa
Antti H <akthurme@gmail.com>
2023-02-22 14:18:29 +0200

    fixed a double import

--- budgeting_app/src/components/Sidebar.js
+++ budgeting_app/src/components/Sidebar.js
@@ -3,14 +3,6 @@ import styled from '@emotion/styled';
 import {SidebarData} from './SidebarData';
 import Submenu from './SubMenu';
 // import * as IconsMaterial from '@mui/icons-material/'; // malli importata kaikki ikonit
-import Submenu from "./SubMenu";
-import Login from "./Login";
-import CreateBankAcc from "./CreateBankAcc";
-import AddCircleOutline from "@mui/icons-material/AddCircleOutline";
-
-
-
-
 
 const SidebarWrap = styled.nav`
   width: 100%;
