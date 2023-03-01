fe52ec7f49cdec07da3580a5b0978b36173e7ac8
Sami Kukkonen <samikukkonen00@hotmail.com>
2023-03-02 01:27:24 +0200

    category and subcategory select queries

--- routes/category.js
+++ routes/category.js
@@ -20,23 +20,41 @@ router.get('/:id', async (req, res) => {
 /**
  * Find users all categories and them subcategories
  */
-router.get('/:id', async (req, res) => {
+router.get('/:id/return-category-dictionary', async (req, res) => {
   try {
+    const userID = req.params.id;
+    const sqlQueryCategories = `SELECT category.CategoryName FROM category WHERE UserID=?`;
+    const categories = await pool.query(sqlQueryCategories, userID);
+    let dictionary = [];
 
+    for(let x = 0; x < categories.length; x++){
+      let subCategoriesList = [];
+      let categoryName = categories[x].CategoryName
+      let categoryBalance = 0;
 
-    const sqlQuery = `SELECT CategoryName FROM category WHERE UserID=?`;
-    const rows = await pool.query(sqlQuery, req.params.id);
-    res.status(200).json(rows);
+      const sqlQuerySubCategories = `SELECT subcategory.SubCategoryName, subcategory.Balance FROM subcategory WHERE subcategory.CategoryID = (SELECT category.CategoryID FROM category WHERE category.CategoryName = '${categoryName}' AND category.UserID = ${userID})`;
+      const subCategories = await pool.query(sqlQuerySubCategories);
+
+      for(let y = 0; y < subCategories.length; y++){
+        let subcategoryBalance = Number(subCategories[y].Balance);
+        subCategoriesList.push([
+            subCategories[y].SubCategoryName, subcategoryBalance
+        ]);
+        categoryBalance+= subcategoryBalance;
+      }
+
+      dictionary.push({
+        category : categoryName,
+        balance : categoryBalance,
+        subcategory : subCategoriesList,
+      });
+    }
+
+    res.status(200).json(dictionary);
   } catch (error) {
     res.status(400).send(error.message);
   }
 });
-
-
-
-
-
-
 
 /**
  * Add new category
