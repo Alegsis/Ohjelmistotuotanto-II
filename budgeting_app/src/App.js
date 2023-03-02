472cdac4375d138366e1a917ed7662d88e4fcfcb
Aleksi Virtanen <aleksvir@uef.fi>
2023-03-02 21:14:55 +0200

    vähän vielä siistitty

--- budgeting_app/src/App.js
+++ budgeting_app/src/App.js
@@ -7,9 +7,6 @@ import Accounts from './pages/Accounts';
 import {useState} from 'react';
 import Account from "./pages/Account";
 
-/*
-* Huom, jos haluat saada oikean sarakkeen näkymään, poista kommentit divistä*/
-
 function App() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const [loggedIn, setLoggedIn] = useState(false);
@@ -18,27 +15,21 @@ function App() {
       setIsSidebarOpen(!isSidebarOpen)
     }
 
-
   return (
       <Router>
         <div className="header">
-          <Header setIsSidebarOpen={setIsSidebarOpen}
-                  toggleSidebar={toggleSidebar}
-                  loggedIn={loggedIn}
-                  setLoggedIn={setLoggedIn}/>
+          <Header setIsSidebarOpen={setIsSidebarOpen} toggleSidebar={toggleSidebar} loggedIn={loggedIn} setLoggedIn={setLoggedIn}/>
         </div>
         <div className="row">
           <div className={`column left ${isSidebarOpen ? '' : 'hidden'}`}>
             <Sidebar/>
           </div>
           <div className={`column middle ${loggedIn ? '' : 'hidden'}`}>
-              {loggedIn && (
               <Routes>
               <Route path="/dashboard" element={<Dashboard/>}/>
               <Route path="/accounts" element={<Accounts/>}/>
               <Route path={`/accounts/:AccountName`} element={<Account/>}/>
             </Routes>
-              )}
           </div>
           {/*<div className="column right">*/}
           {/*  <p> Oikea sarake</p>*/}
--- budgeting_app/src/pages/Account.js
+++ budgeting_app/src/pages/Account.js
@@ -2,7 +2,6 @@ import React from 'react';
 import AccountTransactionGrid from "../components/account/AccountTransactionGrid";
 import {useParams} from "react-router-dom";
 
-
 const Account = () => {
     const { AccountName } = useParams();
 
