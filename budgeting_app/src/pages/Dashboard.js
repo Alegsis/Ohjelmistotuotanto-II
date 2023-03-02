6deb47a34ad98de55641cfce0f0f107cb919fb61
Aleksi134 <aleleskinen11@gmail.com>
2023-03-02 13:16:11 +0200

    first commit

--- budgeting_app/src/pages/Dashboard.js
+++ budgeting_app/src/pages/Dashboard.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import AddCategory from "../components/categories/AddCategory";
+import AddSubCategory from "../components/categories/AddSubCategory";
 
 const Dashboard = () => {
-    return (
-        <div className ='home'>
-            <h1>Dashboard</h1>
-        </div>
-    );
+  return (
+    <div className="home">
+      <AddCategory></AddCategory>
+      <AddSubCategory></AddSubCategory>
+    </div>
+  );
 };
 
 export default Dashboard;
